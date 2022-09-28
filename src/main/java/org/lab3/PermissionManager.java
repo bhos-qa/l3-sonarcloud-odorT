0a3dfb05c99aa24e71b1f94d3ab35d6d1ffdfaf8
kamran.n.karimov <kamran.n.karimov@abb-bank.az>
2022-09-28 10:45:58 +0400

    WIP: sonarqube check 17

--- src/main/java/org/lab3/PermissionManager.java
+++ src/main/java/org/lab3/PermissionManager.java
@@ -26,3 +26,25 @@ public class PermissionManager {
         this.mCurrentLevel = mCurrentLevel;
     }
 }
+
+class Isgandar {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    private String surname;
+}
