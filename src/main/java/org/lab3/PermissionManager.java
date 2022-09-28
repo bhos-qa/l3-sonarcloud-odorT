039d8723726e2a73d11f5bdafd6e9d6145c9e3a6
kamran.n.karimov <kamran.n.karimov@abb-bank.az>
2022-09-28 11:15:49 +0400

    WIP: sonarqube check 18

--- src/main/java/org/lab3/PermissionManager.java
+++ src/main/java/org/lab3/PermissionManager.java
@@ -10,41 +10,14 @@ public class PermissionManager {
     private PermissionLevel mCurrentLevel = PermissionLevel.USER;
 
     public String getmCurrentLevel() {
-        switch (this.mCurrentLevel) {
-            case USER:
-                return "USER";
-            case ADMIN:
-                return "ADMIN";
-            case DEVELOPER:
-                return "DEVELOPER";
-            default:
-                return null;
-        }
+        return switch (this.mCurrentLevel) {
+            case USER -> "USER";
+            case ADMIN -> "ADMIN";
+            case DEVELOPER -> "DEVELOPER";
+        };
     }
 
     public void setmCurrentLevel(PermissionLevel mCurrentLevel) {
         this.mCurrentLevel = mCurrentLevel;
     }
-}
-
-class Isgandar {
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    private String name;
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    private String surname;
 }
